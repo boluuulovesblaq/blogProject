1e5e30f0ef669f4fc06278b36fcc53f2458ea9d0
Big Champ <boluuulovesblack@gmail.com>
2026-04-21 13:43:32 +0100

    your message

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField( max_length= 200)
+    slug = models.SlugField()
+    body = models.TextField()
+    dateTime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def snippet(self):
+        if len(self.body) > 50:
+            return self.body[:50] + '...'
+        else:
+            return self.body
